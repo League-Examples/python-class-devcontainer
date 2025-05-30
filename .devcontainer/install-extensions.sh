11dbc7dfe796f0aded34628626c6824f20e697b2
Eric Busboom <eric@civicknowledge.com>
2025-05-30 21:53:18 +0000

    Update devcontainer configuration to include JTL Syllabus extension and enhance post-creation script

---
+++ .devcontainer/install-extensions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing VS Code extensions..."
+code --install-extension "/vscode/extensionsCache/jtl-syllabus.vsix"
+echo "Done!"
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,16 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-ext_url='https://github.com/league-infrastructure/league-vscode-ext/releases/download/v1.20250530.1/jtl-syllabus-v1.20250530.1.vsix'
+ext_url='https://github.com/league-infrastructure/league-vscode-ext/releases/download/v1.20250530.2/jtl-syllabus-v1.20250530.2.vsix'
 ext_path=/vscode/extensionsCache/jtl-syllabus.vsix
 
+# Download the extension
 curl -L -o "$ext_path" "$ext_url"
 if [ -f "$ext_path" ]; then
-    echo "Installing JTL Syllabus extension..."
-    code --install-extension "$ext_path"
+    echo "Downloaded JTL Syllabus extension to $ext_path"
+
 else
     echo "Failed to download JTL Syllabus extension."
 fi
 
+# Install the Python package regardless
 pipx install jtl-syllabus
