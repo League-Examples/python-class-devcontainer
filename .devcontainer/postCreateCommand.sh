6c218322336062709173849c7eb781241e2968ed
Eric Busboom <eric@civicknowledge.com>
2025-05-30 21:07:40 +0000

    .

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
-ext_ur='https://github.com/league-infrastructure/league-vscode-ext/releases/download/v1.20250510.3/jtl-syllabus-v1.20250510.3.vsix'
+ext_url='https://github.com/league-infrastructure/league-vscode-ext/releases/download/v1.20250530.1/jtl-syllabus-v1.20250530.1.vsix'
 ext_path=/vscode/extensionsCache/jtl-syllabus.vsix
 
-curl -L -o "$ext_path" "$ext_ur"
+curl -L -o "$ext_path" "$ext_url"
 if [ -f "$ext_path" ]; then
     echo "Installing JTL Syllabus extension..."
     code --install-extension "$ext_path"
