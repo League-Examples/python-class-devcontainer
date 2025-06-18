2dac74538fe48fe4acd011e9b457de2523c2e038
Eric Busboom <eric@civicknowledge.com>
2025-06-18 23:14:36 +0000

    Fix exercise paths in syllabus.yaml and update pip installation command in postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,6 @@ python3 -m pip install --upgrade pip
 
 # Check if requirements.txt exists in the root of the workspace
 if [ -f "requirements.txt" ]; then
-    pip3 install -r requirements.txt
-
-else
+    pip3 install --user -r requirements.txt
+fi
 
